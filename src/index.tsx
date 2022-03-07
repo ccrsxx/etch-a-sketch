1c63255cbfdb02f2de56f43a6ee146ffc7008cf2
ccrsxx <aminrisal@gmail.com>
2022-03-07 21:19:17 +0700

    all done!

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
 
 const container = document.getElementById('root') as HTMLDivElement;
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 
 root.render(
   <StrictMode>
